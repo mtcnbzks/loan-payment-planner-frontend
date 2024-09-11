444496592e1e4578616001071a118b7512defca7
mtcnbzks <mtcnbzks@gmail.com>
2024-09-11 13:27:33 +0300

    Add comments

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,18 +1,18 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-function Loader() {
-  useEffect(() => {
-    async function getLoader() {
-      const { leapfrog } = await import("ldrs");
-      leapfrog.register();
-    }
-    getLoader();
-  }, []);
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <l-leapfrog size="40" speed="2.2" color="white"></l-leapfrog>
-    </div>
-  );
-}
+// function Loader() {
+//   useEffect(() => {
+//     async function getLoader() {
+//       const { leapfrog } = await import("ldrs");
+//       leapfrog.register();
+//     }
+//     getLoader();
+//   }, []);
+//   return (
+//     <div className="flex items-center justify-center h-screen">
+//       <l-leapfrog size="40" speed="2.2" color="white"></l-leapfrog>
+//     </div>
+//   );
+// }
